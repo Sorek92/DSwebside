4499f7328555d332b57dc10905407dc6ff73bc61
Dominik <dominiksar92@gmail.com>
2019-03-26 17:13:05 +0100

    Finish rwd navigation
    Connect nav branch to master

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -7,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
+  // english nav, if polish change
+  title = {
+    about: "about",
+    school: "school",
+    experiance: "experiance",
+    skills: "skills",
+    projects: "projects",
+    interests: "interests",
+    contact: "contact"
+
+  }
+
   constructor() { }
 
   ngOnInit() {
