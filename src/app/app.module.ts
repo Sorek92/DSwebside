d875d74274afa8ae23840a74d0182229abb1e07f
Dominik <dominiksar92@gmail.com>
2019-04-08 21:37:05 +0200

    Add home page and settings for rwd
    Change file from home.component.sass to home.component.scss
    Add route '/' like home
    Modify style for navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +15,8 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Content } from '@angular/compiler/src/render3/r3_ast';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.sass']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  content = {};
+
+  contentPL = {
+    title: 'Witam na mojej stronie',
+    text: "Cześć, jestem Dominik. Mam 26 lat. Studiowałem na Politechnice Rzeszowskiej. Moje pierwsze kroki w programowaniu zacząłem w c ++. Później nauczyłem się technologii: pascal, php, c, java, sql, html, css. Teraz próbuję dowiedzieć się więcej o typescript(Angular), javascript (nodejs) i css (sass).",
+    questionnaire: 'Proszę kliknąć jeśli spodobał Ci się wygląd strony'
+  }
+
+
+  contentEN = {
+    title: 'Welcome on my webside',
+    text: "Hi, I'm Dominik. I'm 26 years old. I was studying on Rzeszow University of Technology. My first steps in programming I started in c++. Later I learned technologies: pascal, php, c, java, sql, html, css.  Now I try learn more about typescript(Angular), javascript(nodejs) and css(sass).",
+    //text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    questionnaire: 'Please click if you like thats web design'
+  }
+
+
+  constructor() { 
+    this.content = this.contentEN;
+  }
 
   ngOnInit() {
   }
