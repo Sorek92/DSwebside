393dc4584ecf0763eee87578305b755221332775
Dominik <dominiksar92@gmail.com>
2019-04-08 21:43:27 +0200

    Add new components for routes
    route about
    files : html, sass, spec.ts, ts
    
    route experiance
    files : html, sass, spec.ts, ts
    
    route school
    files : html, sass, spec.ts, ts
    
    route skills
    files : html, sass, spec.ts, ts
    
    route projects
    files : html, sass, spec.ts, ts
    
    route interests
    files : html, sass, spec.ts, ts
    
    route contact
    files : html, sass, spec.ts, ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { SchoolComponent } from './school/school.component';
+import { ExperianceComponent } from './experiance/experiance.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { InterestsComponent } from './interests/interests.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    SchoolComponent,
+    ExperianceComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    InterestsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
