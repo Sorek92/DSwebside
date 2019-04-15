aea1651212db9afd576a2a651811d775c3d4428e
Dominik <dominiksar92@gmail.com>
2019-04-15 09:21:47 +0200

    Break in project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ExperianceComponent } from './experiance/experiance.component';
+import { SchoolComponent } from './school/school.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { InterestsComponent } from './interests/interests.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent}
+  {path: '', component: HomeComponent},
+
 ];
 
 @NgModule({
